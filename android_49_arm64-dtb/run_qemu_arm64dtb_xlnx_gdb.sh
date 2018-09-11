1f2fa313292ae25ab73ec3065020122254528c5d
Jianxin <jianxin.pan@amlogic.com>
2018-09-11 18:54:46 +0800

    add arm64 dtb img
    Signed-off-by: Jianxin <jianxin.pan@amlogic.com>

---
+++ android_49_arm64-dtb/run_qemu_arm64dtb_xlnx_gdb.sh
@@ -0,0 +1,10 @@
+mkdir -p share_dir
+qemu-system-aarch64  -machine xlnx-zcu102   -nographic  -kernel ./Image -dtb ./zynqmp-ep108.dtb   --append "rdinit=/linuxrc root=/dev/ram console=ttyPS0 earlycon" -S -s
+
+
+
+#host
+#mkdir share_dir
+#board
+#mkdir /tmp/host_files
+#mount -t 9p -o trans=virtio,version=9p2000.L hostshare /tmp/host_files
